1fc5e859a0513d965aac521520fc70ce605bd55b
QxAxQ <xuxingda42b@gmail.com>
2026-03-31 20:29:31 +0800

    一级分类-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import {getCategoryAPI} from '@/apis/category'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { getBannerAPI } from '@/apis/home';
 
 const categoryData = ref({})
 const route = useRoute()
@@ -11,6 +12,20 @@ const getCategory = async()=>{
 }
 
 onMounted(()=>getCategory())
+
+//获取Banner
+const bannerList = ref([])
+
+const getBanner = async()=>{
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  console.log(res);
+  bannerList.value = res.data.result
+}
+
+
+onMounted(()=>getBanner())
 </script>
 
 <template>
@@ -23,12 +38,21 @@ onMounted(()=>getCategory())
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+       <div class="home-banner">
+        <el-carousel height="500px">
+        <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
     </div>
   </div>
 </template>
 
 
 <style scoped lang="scss">
+
 .top-category {
   h3 {
     font-size: 28px;
@@ -104,6 +128,16 @@ onMounted(()=>getCategory())
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+.home-banner {
+  width: 1240px;
+  height: 500px;
+ margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
