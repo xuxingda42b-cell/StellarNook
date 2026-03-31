f91a725a588412f9d7cbbdc9eb1174cdc4bb503e
QxAxQ <xuxingda42b@gmail.com>
2026-03-31 20:53:23 +0800

    一级分类-激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import {getCategoryAPI} from '@/apis/category'
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { getBannerAPI } from '@/apis/home';
+import GoodsItem from '../Home/components/GoodsItem.vue';
 
 const categoryData = ref({})
 const route = useRoute()
@@ -43,9 +44,28 @@ onMounted(()=>getBanner())
         <el-carousel height="500px">
         <el-carousel-item v-for="item in bannerList" :key="item.id">
         <img :src="item.imgUrl" alt="">
-      </el-carousel-item>
-    </el-carousel>
-  </div>
+        </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div class="sub-list">
+      <h3>全部分类</h3>
+       <ul>
+        <li v-for="i in categoryData.children" :key="i.id">
+         <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
