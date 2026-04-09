b9653b4aab9a29706253360bdf51282b613a5c66
QxAxQ <xuxingda42b@gmail.com>
2026-04-09 17:35:05 +0800

    列表购物车-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -30,24 +30,38 @@ export const useCartStore = defineStore('cart', () => {
     cartList.value.splice(idx, 1)
     //2. 使用数组的过滤方法 - filter
   }
+
   //单选功能
   const singleCheck = (skuId, selected) => {
     //通过skuId找到要修改的那一项，然后把它的selected修改为传过来的selected
     const item = cartList.value.find((item) => item.skuId === skuId)
     item.selected = selected
   }
+
+  //全选功能
+  const allCheck = (selected) => {
+    //把cartList中的每一项的selected都设置为当前的全选框状态
+    cartList.value.forEach((item) => item.selected = selected)
+  }
+
   // 计算属性
   //1. 总的数量 所有项的count之和
   const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
   //2. 总价 所有项的count*price之和
   const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
+  //是否全选
+  const isAll = computed(() => cartList.value.every((item) => item.selected))
+
   return {
     cartList,
     allCount,
     allPrice,
+    isAll,
     addCart,
     delCart,
-    singleCheck
+    singleCheck,
+    allCheck
   }
 }, {
   persist: true,
